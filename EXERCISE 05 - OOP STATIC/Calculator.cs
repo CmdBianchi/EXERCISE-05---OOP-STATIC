c3048d96a7d4ee04dd972af38a1dfa29681763cb
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-26 14:43:23 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 05 - OOP STATIC/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EXERCISE_05___OOP_STATIC{
+    //----------------------START | OBJ RETANGULO------------------------//
+    class Calculator {
+        public static double Pi = 3.14;
+        public static double Cicle(double r) {
+            return 2 * Pi * r;
+        }
+        public static double Volume(double r) { 
+            return ( 4/3)*Pi*r*r*r;
+        }
+    }
+    //--------------------------- END --------------------------------//  
+}
---
+++ EXERCISE 05 - OOP STATIC/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace EXERCISE_05___OOP_STATIC{
+    //-----> INICIO
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    class Program {      
+        static void Main(string[] args) {
+            Console.Write("Entre com o valor do Raio: "); double radius = double.Parse(Console.ReadLine());
+            double circ = Calculator.Cicle(radius);
+            double volume = Calculator.Volume(radius);
+
+            Console.Write("Valor da circunferencia: "+circ.ToString("F2"));
+            Console.WriteLine();
+            Console.Write("Valor do Volume: "+volume.ToString("F2"));
+            Console.WriteLine();
+            Console.Write("Valor de PI: "+Calculator.Pi.ToString("F2"));
+
+        }
+        
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> FIM
+}
